f48e03253ddc17ee6aa256fb88d5e307d104019c
annin-tofu <yuok5770@gmail.com>
2021-06-11 02:33:47 +0900

    second commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,7 +23,7 @@ const Footer = ({ data }) => {
             <li>
               Made by{" "}
               <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+                Yuya Okamura
               </a>
             </li>
           </ul>
