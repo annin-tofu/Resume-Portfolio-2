32c51c388987a6c739878cd784b65f74b19496d6
annin-tofu <yuok5770@gmail.com>
2021-06-16 02:58:01 +0900

    bug fix

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,7 +22,7 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
+              <a title="Yuya" href="resume-portfolio-2.vercel.app/">
                 Yuya Okamura
               </a>
             </li>
