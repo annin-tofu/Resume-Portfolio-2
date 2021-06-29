0473ea0e27ceb586267f4b1cf61763c8c7f95349
annin-tofu <yuok5770@gmail.com>
2021-06-29 10:19:37 +0900

    Added Education, removed Work

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -54,16 +54,6 @@ const Resume = ({ data }) => {
         </div>
       </div>
 
-      <div className="row work">
-        <div className="three columns header-col">
-          <h1>
-            <span>Work</span>
-          </h1>
-        </div>
-
-        <div className="nine columns main-col">{work}</div>
-      </div>
-
       <div className="row skill">
         <div className="three columns header-col">
           <h1>
